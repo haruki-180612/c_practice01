cd46b798674c086ba7472d5d18ac1ccb0a58ebe2
haruki-180612 <p4n66ziu@s.okayama-u.ac.jp>
2026-04-07 12:06:04 +0900

    Add files via upload

---
+++ kadai1_09B25458_2.c
@@ -0,0 +1,36 @@
+/* 09b25458 沖本陽樹*/
+#include<stdio.h>
+
+int main(void){
+    int i,j,n,x;
+
+    //入力
+    printf("Input n\n");
+    scanf("%d",&n);
+
+    //上の行
+    printf("    |");
+    for(i=n;i>=1;i--) {
+        printf("%3d",i);
+    }    
+    printf("\n");
+
+    //横枠線
+    printf(" ---+");
+    for(i=n;i>=1;i--) {
+        printf("---");
+    }
+    printf("-\n");
+
+    //表
+    for(i=n;i>=1;i--) {
+        printf("%3d |",i);
+        for(j=n;j>=1;j--) {
+            x = i*j;
+            printf("%3d",x);
+        }
+        printf("\n");
+    }
+
+return 0;
+}
